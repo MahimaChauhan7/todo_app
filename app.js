a24cce5d4c5efe48ec4b8066de48d8081b5b21e6
MahimaChauhan7 <156904119+MahimaChauhan7@users.noreply.github.com>
2025-05-31 00:02:19 +0530

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express'); 
 const app = express() ;
 app.use(express.json()); // This will help to parse the json file automatically to req.body when required 
@@ -46,10 +45,52 @@ app.put("/todos/up/:id", (req, res) =>{
         todo: todo[index] 
     });
 
-
-
-
 })
+// delete the todo 
+app.delete('/delete/:id', (req,res) =>{
+    // first fetch the todo 
+   
+    const id = parseInt(req.params.id);
+    //get the index of the task 
+    const indx = todo.findIndex(item => item.id ===id); 
+    if(indx ===-1 ){
+        res.status(404).json({
+            success: false,
+            message: "Todo not found"
+        })
+    }
+    // use splice 
+    const del = todo.splice(indx, 1)[0]; 
+    res.status(200).json({
+        success: true,
+        message: "Your todo is deleted"
+    }); 
+
+});
+// read the file with the given id 
+app.get("/todos/read/:id", (req, res) =>{
+    // first fetch the id 
+    const id = parseInt(req.params.id); 
+    const indexx = todo.findIndex(item => item.id === id); 
+    if(indexx === -1){
+        return res.status(404).json({
+            message: "Todo not found"
+        });
+        
+    }
+    res.send(todo[indexx]); 
+
+});
+app.get("/todos/read/all", function (req, res) {
+    
+    if (todo.length === 0) {
+        return res.send("No todos found");
+    }
+
+    // send the todos array as response
+    res.send(todo);
+});
+
 
 app.listen(5050, () =>{
     console.log("Server is running on the port 5050")
